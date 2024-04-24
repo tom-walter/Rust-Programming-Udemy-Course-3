ebcb55e71dc17fe64bdf0aa4bf24dd128f5be598
Tom Walter <tom.k.walter@gmail.com>
2024-04-24 08:54:38 +0200

    Complete Tasks on Exercise G

---
+++ exercise_g/src/main.rs
@@ -0,0 +1,78 @@
+#![allow(unused_variables, unused_mut, dead_code)]
+
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
+
+impl Shot {
+    fn points(&self) -> i32 {
+        match *self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) if x < 3.0 => 2,
+            Shot::Hit(x) => 1,
+            Shot::Miss => 0,
+        }
+    }
+}
+
+fn main() {
+    let arrow_coords: Vec<Coord> = get_arrow_coords(5);
+    let mut shots: Vec<Shot> = Vec::new();
+
+    for coord in arrow_coords {
+        coord.print_description();
+        let shot = match coord.distance_from_center() {
+            x if x < 1.0 => Shot::Bullseye,
+            x if x < 5.0 => Shot::Hit(x),
+            _ => Shot::Miss,
+        };
+
+        shots.push(shot);
+    }
+
+
+    // let mut total = 0;
+    // for shot in shots {
+    //     total += shot.points();
+    // }
+
+    let total = shots.iter().map(|s| s.points()).fold(0, |sum, points| sum + points);
+
+    println!("Final point total is: {}", total);
+}
+
+// A coordinate of where an Arrow hit
+#[derive(Debug)]
+struct Coord {
+    x: f64,
+    y: f64,
+}
+
+impl Coord {
+    fn distance_from_center(&self) -> f64 {
+        (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
+    }
+    fn print_description(&self) {
+        println!(
+            "coord is {:.1} away, at ({:.1}, {:.1})",
+            self.distance_from_center(),
+            self.x,
+            self.y);
+    }
+
+}
+
+// Generate some random coordinates
+fn get_arrow_coords(num: u32) -> Vec<Coord> {
+    let mut coords: Vec<Coord> = Vec::new();
+    for _ in 0..num {
+        let coord = Coord {
+            x: (rand::random::<f64>() - 0.5) * 12.0,
+            y: (rand::random::<f64>() - 0.5) * 12.0,
+        };
+        coords.push(coord);
+    }
+    coords
+}
